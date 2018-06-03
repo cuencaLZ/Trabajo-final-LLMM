5a3b9985bbf7d338b87105cf1cc4111e63f6ff3e
Lazarocp <lazaro.cuenca8@gmail.com>
2018-06-04 01:23:33 +0200

    dar formato al documento

--- js/news.js
+++ js/news.js
@@ -3,60 +3,60 @@ var activado = false;
 var theme = 0;
 
 $(window).scroll(function () {
-    if ($(window).scrollTop() + $(window).height() + 10 >= $(document).height() && activado == true) {
-        load();
-    }
+	if ($(window).scrollTop() + $(window).height() + 10 >= $(document).height() && activado == true) {
+		load();
+	}
 });
 
 function load() {
-    if (loaded < 3) {
+	if (loaded < 3) {
 		$("#loading").show();
-        $.getJSON("https://rawgit.com/cuencaLZ/Trabajo-final-LLMM/master/data/" + loaded + ".json", function (jsonObject) {
-            addrow(jsonObject);
+		$.getJSON("https://rawgit.com/cuencaLZ/Trabajo-final-LLMM/master/data/" + loaded + ".json", function (jsonObject) {
+			addrow(jsonObject);
 			$("#loading").hide();
-        }); 
+		});
 		loaded++;
 		if (loaded > 2) {
 			$("#loadButton").html("No hay más noticias.");
 		}
-    }
+	}
 };
 
 function addrow(json) {
-    $.each(json, function (i, item) {
-        $("#loadedNews").append(
-            '<article class="col-xs-12 col-sm-10 col-md-10">'+
-		'<div class="añadir row">' +
-            '<div class="fecha">' +
-				'<div class="well well-sm">' + 
-					item.date +
-				'</div>' +
-				'<img src="' + item.imgMid + '" class="img-rounded Notimagen" id="' + item.imgId + '" data-toggle="modal" data-target="#newModal" alt="..."/>' +
-			'</div>' +	
-            '<div class="descripcion">' + 
-					'<h3>'+
-					item.title  + 
-				'</h3>' +
-				'<p>' + 
-					item.description + 
-				'</p>' + 
-				'<a href="news.html">'+
-                            'Leer mas...'+
-                '</a>'+
-			'</div>' + 
-        '</div>'+
-        '</article>');
-    })
+	$.each(json, function (i, item) {
+		$("#loadedNews").append(
+			'<article class="col-xs-12 col-sm-10 col-md-10">' +
+			'<div class="añadir row">' +
+			'<div class="fecha">' +
+			'<div class="well well-sm">' +
+			item.date +
+			'</div>' +
+			'<img src="' + item.imgMid + '" class="img-rounded Notimagen" id="' + item.imgId + '" data-toggle="modal" data-target="#newModal" alt="..."/>' +
+			'</div>' +
+			'<div class="descripcion">' +
+			'<h3>' +
+			item.title +
+			'</h3>' +
+			'<p>' +
+			item.description +
+			'</p>' +
+			'<a href="news.html">' +
+			'Leer mas...' +
+			'</a>' +
+			'</div>' +
+			'</div>' +
+			'</article>');
+	})
 };
 
-$(document).ready(function(){
-	
+$(document).ready(function () {
+
 	$("#activado").tooltip();
-    $("#desactivado").tooltip();
+	$("#desactivado").tooltip();
 	$("#activado").hide();
 	$("#loading").hide();
-	
-    $(".toggle").click(function(){
+
+	$(".toggle").click(function () {
 		if (activado == false) {
 			$("#desactivado").hide();
 			$("#activado").show();
@@ -68,15 +68,15 @@ $(document).ready(function(){
 			activado = false;
 			$("#loadButton").show();
 		}
-    });
-	
-	
-	$("#loadButton").click(function() {load();});
-		
-	$("body").on("click", ".Notimagen", function(){
+	});
+
+
+	$("#loadButton").click(function () { load(); });
+
+	$("body").on("click", ".Notimagen", function () {
 		var id = $(this).attr('id');
-		$("#modalImg").attr("src","img/imgbig" + id +".jpg");
-    });
-	
-	
+		$("#modalImg").attr("src", "img/imgbig" + id + ".jpg");
+	});
+
+
 });
